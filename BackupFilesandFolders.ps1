968e9daeaa26e10bfb34808207b9bd5f54d11390
Ankith.M <Ankith.M@euromonitor.com>
2020-10-19 17:40:11 +0530

    switch aaded

--- BackupFilesandFolders.ps1
+++ BackupFilesandFolders.ps1
@@ -8,11 +8,30 @@
 ##            3. Removed hardcoded source and destination.
 ##            4. Add user input for Source/Destination location.
 ##            5. More dynamic now.
+##            6. Switch added for multiple operations.
+Write-Host = "Select your operation"
+Write-Host = "1. Robocopy"
+Write-Host = "2. AzCopy"
+[Int16]$choice = Read-Host -Prompt "Please enter your choice. `n" 
 
-$date = $null
-$date = Get-Date -f ddMMyyyyTHHmmss
-[string]$source = Read-Host -Prompt 'Source location'
-[string]$destination = Read-Host -Prompt 'Destination location'
-[string]$log = $destination + "\robocopylog" + $date + ".txt"
-robocopy $source $destination /E /XO > $log
-Write-Host "Copy completed, Check logs at $log for any issue."
+switch ($choice) 
+{
+    1
+    {
+        $date = $null
+        $date = Get-Date -f ddMMyyyyTHHmmss
+        [string]$source = Read-Host -Prompt 'Source location'
+        [string]$destination = Read-Host -Prompt 'Destination location'
+        [string]$log = $destination + "\robocopylog" + $date + ".txt"
+        robocopy $source $destination /E /XO > $log
+        Write-Host "Copy completed, Check logs at $log for any issue."
+    }
+    2
+    {
+        Write-Host = "Coming soon"
+    }
+    Default 
+    {
+        Write-Host = "Choice is 1 or 2"
+    }
+}
