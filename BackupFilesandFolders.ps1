ac5c7b0b05300047fc065de7f3162f97e86afe87
Ankith.M <Ankith.M@euromonitor.com>
2020-10-19 11:42:44 +0530

    Updated script

--- BackupFilesandFolders.ps1
+++ BackupFilesandFolders.ps1
@@ -1,13 +1,14 @@
 ## Version 0.1
 ## Backup Files to external drive using Robocopy
-## ChangeLog: 1. Added to GIT HUB
-##            2. Testing Write_progress
-
+## ChangeLog: 1. Added to GIT HUB.
+##            2. Testing Write_progress.
+##            3. Removed hardcoded source and destination.
 $date=$null
 $date = Get-Date -f ddMMyyyyTHHmmss
-robocopy 'C:\Users\ankith.m\Downloads\' 'G:\Ankith\Work\Downloads\' /E /XO > G:\Ankith\Work\Logs\robocopylog"$date".txt
+robocopy 'Source' 'Destination' /E /XO > G:\Ankith\Work\Logs\robocopylog"$date".txt
 Write-Progress -Activity "Robocopy"
 
 $date = $null
 $date = Get-Date -f ddMMyyyyTHHmmss
-robocopy 'C:\_D Drive\' 'G:\Ankith\Work\_D Drive\' /E /XO > G:\Ankith\Work\Logs\robocopylog"$date".txt
+robocopy 'Source' 'Destination' /E /XO > G:\Ankith\Work\Logs\robocopylog"$date".txt
+Write-Progress -Activity "Robocopy"
