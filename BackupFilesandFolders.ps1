1098104c296a9eea2d98d493a52a719c728a0e92
Ankith.M <Ankith.M@euromonitor.com>
2020-10-19 11:22:11 +0530

    Added commments

--- BackupFilesandFolders.ps1
+++ BackupFilesandFolders.ps1
@@ -1,7 +1,7 @@
 ## Version 0.1
 ## Backup Files to external drive using Robocopy
-## ChangeLog:
-
+## ChangeLog: 1. Added to GIT HUB
+##            2.
 $date=$null
 $date = Get-Date -f ddMMyyyyTHHmmss
 robocopy 'C:\Users\ankith.m\Downloads\' 'G:\Ankith\Work\Downloads\' /E /XO > G:\Ankith\Work\Logs\robocopylog"$date".txt
