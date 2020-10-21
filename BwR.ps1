4276e40081686d337a982cf2d8452cfb4ee4ef22
Ankith M <Ankith.M@Euromonitor.com>
2020-10-21 17:14:15 +0530

    Changelog removed

--- BwR.ps1
+++ BwR.ps1
@@ -2,13 +2,6 @@
 ## Created by: Ankith Mankunnu
 ## Backup Files to external drive using Robocopy
 ## Usage: Call the script from PS console or IDE.
-## ChangeLog: 1. Added to GIT HUB.
-##            2. Testing Write_progress.
-##            3. Removed hardcoded source and destination.
-##            4. Add user input for Source/Destination location.
-##            5. More dynamic now.
-##            6. Switch added for multiple operations.
-##            7. Changed log file extention to .log.
 
 Write-Host = "Select your operation"
 Write-Host = "1. Robocopy"
