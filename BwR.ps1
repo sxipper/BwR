cd0c339db4f316dca816a2c8d740f037b7c7e906
Ankith Mankunnu <7589060+sxipper@users.noreply.github.com>
2021-06-03 13:51:16 +0530

    Update BwR.ps1

--- BwR.ps1
+++ BwR.ps1
@@ -1,4 +1,4 @@
-## Version 0.9
+## Version 0.9.1
 ## Created by: Ankith Mankunnu
 ## Backup Files to external drive using Robocopy or other options.
 ## Usage: Call the script from PS console or IDE.
@@ -7,34 +7,45 @@ Write-Host = "Select your operation"
 Write-Host = "1. Robocopy"
 Write-Host = "2. AzCopy"
 Write-Host = "0. Clear/Exit"
-[Int16]$choice = Read-Host -Prompt "Please enter your choice. `n" 
-
-switch ($choice) 
+$choice = Read-Host -Prompt "Please enter your choice. `n" 
+if($choice -match '^[0-9]+$')
 {
-    1
+    switch ($choice) 
     {
-        $date = $null
-        $date = Get-Date -f ddMMyyyyTHHmmss
-        $source = Read-Host -Prompt 'Source location'
-        $destination = Read-Host -Prompt 'Destination location'
-            if ([string]::IsNullOrEmpty($source) -or [string]::IsNullOrEmpty($destination) -or [string]::IsNullOrWhiteSpace($source) -or [string]::IsNullOrWhiteSpace($destination))
-            {
-                Write-Host "Source or Destination is empty" -ForegroundColor Red
-                Start-Sleep -Seconds 3
-                Clear-Host
-            }
-            else
-            {
-                [string]$log = $destination + "\robocopylog" + $date + ".log"
-                New-Item $log -ItemType File -Force -Verbose | Out-Null
-                robocopy $source $destination /E /XO /LOG:$log /TEE
-                Write-Host "Copy completed, Check $log for any issue." -ForegroundColor Green
-            }
+        1
+        {
+            $date = $null
+            $date = Get-Date -f ddMMyyyyTHHmmss
+            $source = Read-Host -Prompt 'Source location'
+            $destination = Read-Host -Prompt 'Destination location'
+            $path = @($source, $destination)
+                if ([string]::IsNullOrEmpty($source) -or [string]::IsNullOrEmpty($destination) -or [string]::IsNullOrWhiteSpace($source) -or [string]::IsNullOrWhiteSpace($destination))
+                {
+                    Write-Host "Source or Destination is empty" -ForegroundColor Red
+                    Start-Sleep -Seconds 3
+                    Clear-Host
+                }
+                elseif(($source | Test-path -PathType Any) -or ($destination | Test-path -PathType Any))
+                {
+                    [string]$log = $destination + "\robocopylog" + $date + ".log"
+                    New-Item $log -ItemType File -Force -Verbose | Out-Null
+                    robocopy $source $destination /E /XO /LOG:$log /TEE
+                    Write-Host "Copy completed, Check $log for any issue." -ForegroundColor Green
+                }
+                else
+                {
+                    Write-Host "Path entered could be wrong" -ForegroundColor Red
+                    Start-Sleep -Seconds 3
+                    Clear-Host
+                }
+        }
+        2
+        {Write-Host "Coming soon!!" -ForegroundColor Yellow}
+        0
+        {Clear-Host}
+        Default 
+        {Write-Host "Wrong Operation" -ForegroundColor Red}
     }
-    2
-    {Write-Host = "Coming soon"}
-    0
-    {Clear-Host}
-    Default 
-    {Write-Host = "Wrong Operation"}
 }
+else
+{Write-Host "Invalid choice" -ForegroundColor DarkRed -BackgroundColor Yellow}
