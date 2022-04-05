bb2336651cde8405773bdd8268d26a2d34eb32f0
ankith.m <Ankith.M@euromonitor.com>
2022-04-05 14:36:08 +0530

    Made in par with scripting standards.

--- BwR.ps1
+++ BwR.ps1
@@ -9,7 +9,7 @@ Write-Host = "1. Robocopy"
 Write-Host = "2. AzCopy"
 Write-Host = "0. Clear/Exit"
 $choice = Read-Host "Please enter your choice. `n" 
-if($choice -match '^[0-9]+$')
+if ($choice -match '^[0-9]+$')
 {
     switch ($choice) 
     {
@@ -18,25 +18,25 @@ if($choice -match '^[0-9]+$')
             $date = Get-Date -f ddMMyyHHmmss
             $source = Read-Host 'Source location'
             $destination = Read-Host 'Destination location'
-                if ([string]::IsNullOrEmpty($source) -or [string]::IsNullOrEmpty($destination) -or [string]::IsNullOrWhiteSpace($source) -or [string]::IsNullOrWhiteSpace($destination))
-                {
-                    Clear-Host
-                    Write-Host "Source or Destination is empty" -ForegroundColor Red
-                    Start-Sleep -Seconds 5
-                }
-                elseif(($source | Test-path -PathType Any) -or ($destination | Test-path -PathType Any))
-                {
-                    [string]$log = $destination + "\RobocopyLog" + $date + ".log"
-                    New-Item $log -ItemType File -Force | Out-Null
-                    robocopy $source $destination /E /XO /MT /LOG:$log /TEE
-                    Write-Host "Copy completed, Check $log for any issue." -ForegroundColor Green
-                }
-                else
-                {
-                    Clear-Host
-                    Write-Host "Path entered could be wrong" -ForegroundColor Red
-                    Start-Sleep -Seconds 5
-                }
+            if ([string]::IsNullOrEmpty($source) -or [string]::IsNullOrEmpty($destination) -or [string]::IsNullOrWhiteSpace($source) -or [string]::IsNullOrWhiteSpace($destination))
+            {
+                Clear-Host
+                Write-Host "Source or Destination is empty" -ForegroundColor Red
+                Start-Sleep -Seconds 5
+            }
+            elseif (($source | Test-path -PathType Any) -or ($destination | Test-path -PathType Any))
+            {
+                [string]$log = $destination + "\RobocopyLog" + $date + ".log"
+                New-Item $log -ItemType File -Force | Out-Null
+                robocopy $source $destination /E /XO /MT /LOG:$log /TEE
+                Write-Host "Copy completed, Check $log for any issue." -ForegroundColor Green
+            }
+            else
+            {
+                Clear-Host
+                Write-Host "Path entered could be wrong" -ForegroundColor Red
+                Start-Sleep -Seconds 5
+            }
         }
         2
         {
